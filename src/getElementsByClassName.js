5730193259f8010e8962b304117cffc2d5bac6f1
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-05-09 17:00:25 -0700

    All tests passing for getElementsByClassName
    Implemented recursively with element.classList, element.childNodes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,35 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+  
+  
+  //Checks the element [el] and all it's descendants for
+  //the given className (uses recursion)
+  
+  //Probably don't need the [result] variable to be passed, but
+  //easiest this way...can change later if necessary...
+  function getDescendantsByClassName(className, el, result) {
+    
+    //checks current element for class name
+    for (var i=0; i<el.classList.length; i++) {
+      if (el.classList[i] === className) {
+        console.log("TRUE!");
+        result.push(el);
+        break;
+      }
+    }
+    
+    //checks all descendant elements for className
+    for (var i=0; i<el.childNodes.length; i++) {
+      //If a text node, does nothing, since these have no classes and no descendants (I think)
+      if (el.childNodes[i].nodeType !== 3) {
+        result = getDescendantsByClassName(className, el.childNodes[i], result);
+      }
+    }
+    
+    //returns result
+    return result;
+  }
+  return getDescendantsByClassName(className, document.body, []);
+  
 };
